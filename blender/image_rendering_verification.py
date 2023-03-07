4707715d158b4abd68c8f065a874d625d7f99520
vincenzofanizza <v.fanizza@student.tudelft.nl>
2023-03-07 12:01:05 +0100

    updated .blend files to import utilities from  blender_utils.py

--- blender/image_rendering_verification.py
+++ blender/image_rendering_verification.py
@@ -3,60 +3,17 @@ import bpy
 import numpy as np
 import math
 import mathutils
+import sys
+import os
 
-from scipy.spatial.transform import Rotation
+dir = os.path.dirname(bpy.data.filepath)
+if not dir in sys.path:
+    sys.path.append(dir)
 
+import blender_utils as bu
 
-def reset_viewport():
-    '''
-    Delete all objects added to the viewport in a previous session.
-    
-    '''
-    bpy.ops.object.select_all(action = 'SELECT')
-    bpy.ops.object.delete(use_global = False)
-
-def add_primitive_cubes(verification_test = None):
-    '''
-    Add cubes to the viewport required for verification.
-    
-    '''
-    if verification_test == 'image_rendering':
-        # Create central large cube
-        bpy.ops.mesh.primitive_cube_add(location = (0., 0., 0.), size = 4)
-
-        # Create smaller cubes
-        bpy.ops.mesh.primitive_cube_add(location = (2.5, -1.5, -1.5), size = 1)
-        bpy.ops.mesh.primitive_cube_add(location = (-1., 3., -1.), size = 2)
-    else:
-        raise ValueError('verification test not selected')
-
-def quat2eul(quaternion = None, order = 'ZYX'):
-    '''
-    Convert quaternion (scalar-first notation) to euler angles (in radians) assuming a specified rotation sequence.
-
-    '''
-    return quaternion.to_euler(order)
-
-def place_camera(location = None, rotation = None, rot_type = 'euler'):
-    '''
-    Create and add a camera to the scene with a certain location and rotation.
-
-    '''
-    # Create camera
-    bpy.ops.object.camera_add()
-    camera = bpy.data.objects['Camera']
-
-    if rot_type == 'quaternion':
-        # Quaternions are assumed in a scalar-first notation
-        rotation = quat2eul(rotation)
-    elif rot_type != 'euler':
-        raise ValueError('camera rotation type is unknown')
-    
-    camera.location = location
-    camera.rotation_mode = 'ZYX'
-    camera.rotation_euler = np.array([rotation.x, rotation.y, rotation.z])
-
-    return camera
+import imp
+imp.reload(bu)
 
 
 camera_poses = {
@@ -64,12 +21,11 @@ camera_poses = {
     'rotation': np.array([[0.5, 0.5, 0.5, 0.5], [0., 0.0, -0.707107, -0.707107], [0.5, 0.5, -0.5, -0.5], [0.707107, 0.707107, 0., 0.], [0.707107, 0., 0., 0.707107], [1., 0., 0., 0.], [0., 0.707107, -0.707107, 0.], [0., 0.382683, -0.923880, 0.], [0.707107, 0.707107, 0., 0.]])
 }
 
+bu.reset_viewport()
+bu.add_primitive_cubes(verification_test = 'image_rendering')
 
-reset_viewport()
-add_primitive_cubes(verification_test = 'image_rendering')
-
-for i in range(len(camera_poses['location'])):
-    camera = place_camera(location = camera_poses['location'][i], rotation = mathutils.Quaternion(camera_poses['rotation'][i]), rot_type = 'quaternion')
+for i in range(camera_poses['location'].shape[0]):
+    camera = bu.place_camera(location = mathutils.Vector(camera_poses['location'][i]), rotation = mathutils.Quaternion(camera_poses['rotation'][i]), rot_type = 'quaternion')
     camera.data.type = 'ORTHO'
     camera.data.ortho_scale = 20
     
@@ -80,7 +36,7 @@ for i in range(len(camera_poses['location'])):
     scene.render.resolution_x = 512
     scene.render.resolution_y = 512
     scene.render.film_transparent = True
-
+    
     # Setup rendering filepath
     scene.render.image_settings.file_format = 'PNG'
     scene.render.filepath = "//image_rendering_verification/pose_{num}.png".format(num = i+1)
@@ -91,3 +47,4 @@ for i in range(len(camera_poses['location'])):
     bpy.ops.object.select_all(action = 'DESELECT')
     bpy.data.objects['Camera'].select_set(True)
     bpy.ops.object.delete() 
+    
---
+++ blender/pose_labels_verification.py
@@ -0,0 +1,42 @@
+import bpy
+
+import numpy as np
+import math
+import mathutils
+import sys
+import os
+
+dir = os.path.dirname(bpy.data.filepath)
+if not dir in sys.path:
+    sys.path.append(dir)
+
+import blender_utils as bu
+
+import imp
+imp.reload(bu)
+
+
+bu.reset_viewport()
+bu.add_primitive_cubes(verification_test = 'pose_labels')
+
+camera_pose = {
+    'location': mathutils.Vector((5., 0., 0.)),
+    'rotation': mathutils.Euler(np.radians([0., 90., 90.]), 'ZYX')
+}
+rotation_intr = mathutils.Euler((np.radians(0.), np.radians(0.), np.radians(0.)), 'ZYX')
+# METHOD USED TO DETERMINE EXTRINSIC ROTATIONS DOES NOT WORK - USE SCIPY
+rotation_extr = mathutils.Euler((np.radians(0.), np.radians(0.), np.radians(0.)), 'XYZ')
+
+camera = bu.place_camera()
+
+camera = bu.change_camera_pose(camera, location = camera_pose['location'], rotation = camera_pose['rotation'], rot_type = 'euler')
+camera.data.type = 'ORTHO'
+camera.data.ortho_scale = 3
+
+scene = bpy.context.scene
+scene.camera = camera
+
+# Setup the rendering settings
+scene.render.resolution_x = 512
+scene.render.resolution_y = 512
+scene.render.film_transparent = True
