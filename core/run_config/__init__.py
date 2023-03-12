d7e204972de6b9c00b03e9ec915ef5f9c2845e4d
vincenzofanizza <v.fanizza@student.tudelft.nl>
2023-03-12 15:43:25 +0100

    removed empty __init__.py

--- core/run_config/__init__.py
+++ core/run_config/__init__.py
@@ -1,3 +1,3 @@
 # Make default configuration and the method update_config available on a package level
-from .default_config import _C as cfg
-from .default_config import update_config
+from default_config import _C as cfg
+from default_config import update_config
